081645e30cb03fedc49e173539f3827202e1d7b6
Aryan11-byte <aaryansureshnanda@gmail.com>
2026-03-14 13:38:07 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,16 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1emLLZWy-u66kbAjXuN9evYn4tLd9veU",
+  authDomain: "poonam-herbal-touch.firebaseapp.com",
+  projectId: "poonam-herbal-touch",
+  storageBucket: "poonam-herbal-touch.firebasestorage.app",
+  messagingSenderId: "732786739986",
+  appId: "1:732786739986:web:cbe94a1e05704b7f6601cb",
+  measurementId: "G-N5PXRM0FBE"
+};
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyC1emLLZWy-u66kbAjXuN9evYn4tLd9veU",
-    authDomain: "poonam-herbal-touch.firebaseapp.com",
-    projectId: "poonam-herbal-touch",
-    storageBucket: "poonam-herbal-touch.firebasestorage.app",
-    messagingSenderId: "732786739986",
-    appId: "1:732786739986:web:cbe94a1e05704b7f6601cb",
-    measurementId: "G-N5PXRM0FBE"
-  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Initialize Firestore database
+const db = firebase.firestore();
